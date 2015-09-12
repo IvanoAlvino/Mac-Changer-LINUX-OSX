99ff921a803a84edbd865cee89cc695da83ff162
IvanoAlvino <ivano.alvino@gmail.com>
2015-09-12 03:52:03 +0200

    Small changes
    Improved code linearity

--- mac_changer.py
+++ mac_changer.py
@@ -2,20 +2,20 @@ import os
 import random
 
 def get_hex():
-     letters = ['a', 'b', 'c', 'd', 'e', 'f', '0' , '1', '2', '3', '4', '5', '6', '7', '8', '9']
-     return random.choice(letters)
+    return random.choice("abcdef0123456789")
 
 def get_mac():
-     new_mac = ""
-     for i in range(0,5):
-          new_mac += get_hex() + get_hex() + ":"
-     new_mac += get_hex() + get_hex()
-     print new_mac
-  
-command = "sudo ifconfig en1 ether"
+    new_mac = ""
+    for i in range(0,5):
+        new_mac += get_hex() + get_hex() + ":"
+    new_mac += get_hex() + get_hex()
+    return new_mac
+
 print "Old MAC address: ",
 os.system("ifconfig en1 | grep ether")
-new_mac = get_mac()
-os.system(command + " " + new_mac)
+
+# setting new random MAC address
+os.system("sudo ifconfig en1 ether " + new_mac() )
+
 print "\nNew MAC address: ",
 os.system("ifconfig en1 | grep ether")
